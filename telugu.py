fc61d84c3462d4860fee488a6ad9e67db6d420ab
ravi <ravikiranreddy0508@gmail.com>
2024-02-28 01:24:22 +0530

    created multiplication prgm

---
+++ telugu.py
@@ -0,0 +1,10 @@
+def multiplication(num1, num2):
+    return num1 * num2
+
+# Input numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform multiplication and print the result
+result = multiplication(num1, num2)
+print("The product is:", result)
